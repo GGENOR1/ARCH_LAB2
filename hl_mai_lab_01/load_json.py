d658dadd6d27e190fabf532527cbae51b3583d7a
ggenor <rus.yaminov@mail.com>
2024-03-25 18:10:06 +0300

    init

--- hl_mai_lab_01/load_json.py
+++ hl_mai_lab_01/load_json.py
@@ -2,9 +2,6 @@ import pandas as pd
 import json
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, VARCHAR
 # engine = create_engine("postgresql+psycopg2://stud:stud@localhost/archdb", echo = False)
-
-
-# # Определяем схему таблицы
 # metadata = MetaData()
 # users_table = Table('users', metadata,
 #                     Column('id', Integer, primary_key=True, autoincrement=True),
@@ -14,19 +11,13 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, 
 #                     Column('password', VARCHAR(256)),
 #                     Column('email', VARCHAR(256)),
 #                     Column('title', VARCHAR(1024))
-#                     # Добавьте остальные столбцы
+#                   
 #                     )
 
-# # Создаем таблицу в базе данных
 # metadata.create_all(engine)
-
-# # Загружаем данные из JSON в DataFrame
 # df = pd.read_json("hl_mai_lab_01/ExportJson.json")
-
-# # Записываем данные из DataFrame в таблицу в базе данных
 # df.to_sql('users', con=engine, if_exists='replace', index=False)
-# Создаем метаданные
-# Создаем метаданные
+
 metadata = MetaData()
 
 # Создаем таблицу пользователей
@@ -36,34 +27,28 @@ users_table = Table('users', metadata,
                     Column('last_name', String(256), nullable=False),
                     Column('login', String(256), nullable=False),
                     Column('password', String(256), nullable=False),
-                    Column('email', String(256)),
+                    Column('email', String(256),nullable=False),
                     Column('title', String(1024))
                     )
 
-# Подключение к базе данных (замените 'sqlite:///mydatabase.db' на ваше подключение)
-engine = create_engine("postgresql+psycopg2://stud:stud@localhost/archdb", echo = False)
 
-# Создаем соединение
+engine = create_engine("postgresql+psycopg2://stud:stud@localhost/archdb", echo = False)
 conn = engine.connect()
 
-# Чтение данных из JSON файла
 with open('hl_mai_lab_01/ExportJson.json', 'r') as file:
     data = json.load(file)
-
-# Вставка данных в таблицу
 for record in data:
     insert_query = users_table.insert().values(
         first_name=record['first_name'],
         last_name=record['last_name'],
         login=record['login'],
         password=record['password'],
-        email=record.get('email'),  # Если значение отсутствует, то передаем None
-        title=record.get('title')    # Если значение отсутствует, то передаем None
+        email=record.get('email'), 
+        title=record.get('title')    
         
     )
     
     conn.execute(insert_query)
     conn.commit()
 
-# Закрываем соединение
 conn.close()
